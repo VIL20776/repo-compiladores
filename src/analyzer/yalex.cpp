8c74e6d8df9577382d56e9718299065f7b806430
Dáriel <vil20776@uvg.edu.gt>
2024-03-06 17:04:14 -0600

    yalex initialization

--- src/analyzer/yalex.cpp
+++ src/analyzer/yalex.cpp
@@ -3,27 +3,42 @@
 #include <fstream>
 #include <iostream>
 
-#include "algorithms/algorithms.hpp"
-
 namespace analyzer {
     
-    void Yalex::read()
+    void Yalex::read(std::string filename)
     {
+        std::ifstream archivo("archivo.txt");
+
+        if (!archivo.is_open()) {
+            std::cerr << "No se pudo abrir el archivo." << std::endl;
+            return;
+        }
+
+        char ch;
+        while (archivo.eof()) {
+            // Mostrar el carácter leído
+            archivo.get(ch);
+            
+        }
+
+        archivo.close();
+   
         return;
     }
 
-    Yalex::Yalex(std::string path)
+    Yalex::Yalex()
     {
-        char ch = 0;
-        std::ifstream file {};
-        file.open(path, std::ios_base::in);
-        while (file.get(ch)){
-            std::cout << ch;
-            if (file.eof())
-                break;
-        }
-            
-        file.close();
+        std::string yalex_tokens = 
+            "\"let\"#|"
+            "\"rule\"#|"
+            "[\"\\t\\n\\s\"]#|" //delim
+            "\"[a-zA-Z]+#|" //ident
+            "\\\"_+\\\""; //regexp
+
+        std::unique_ptr<algorithms::AutomataCreator> fa_creator {};
+        fa_creator.reset(new algorithms::Direct(yalex_tokens));
+
+        dfa = fa_creator->create_automata();
     }
 
 
--- src/analyzer/yalex.hpp
+++ src/analyzer/yalex.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
 #include <string>
+#include <memory>
+
+#include "algorithms/algorithms.hpp"
 
 namespace analyzer {
 
 class Yalex
 {
 private:
-    char* yal_content;
+    std::unique_ptr<models::Automaton> dfa;
 
-    void read();
+    void read(std::string filename);
     
 public:
-    Yalex(std::string path);
+    Yalex();
 
 
 };
